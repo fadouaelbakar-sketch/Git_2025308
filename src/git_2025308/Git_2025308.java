8ee21753cf67fcae7086fb39eb81175c06d799e4
fadouaelbakar-sketch <fadoua.el.bakar@gmail.com>
2025-12-07 11:23:19 +0100

    My team - no code

---
+++ src/git_2025308/Git_2025308.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package git_2025308;
+
+/**
+ *
+ * @author store one
+ */
+public class Git_2025308 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Fadoua El bakar-2025308");
+    }
+    
+}
